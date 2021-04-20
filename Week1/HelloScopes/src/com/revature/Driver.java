1234aa1fe62f4677763e704a4814c82ecba2fe4c
gattietime <gattietime@gmail.com>
2021-04-20 12:39:40 -0400

    HelloScopes

---
+++ Week1/HelloScopes/src/com/revature/Driver.java
@@ -0,0 +1,72 @@
+package com.revature;
+
+public class Driver {
+	
+	//This is a class scoped variable. We will immediately assign the value 5 at declaration. 
+	static int i = 5;
+	
+	//This is class scoped but is not given a value at declaration, we say therefore it is not initialized.
+	//Uninitialized primitives will have a default value, generally 0. 
+	static int i2;
+	
+	//This is a class scoped reference variable. For uninitialized reference variables the default value is "null".
+	static Driver d; 
+	
+	//This is an instance/object scoped variable. 
+	double d2 = 18.95;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(i);
+		System.out.println(i2);
+		System.out.println(d);
+		//System.out.println(d2); The static method can not see the instance scoped variables. 
+		
+		i2 = -98;
+		
+		d = new Driver();
+		
+		System.out.println("=================================================");
+		System.out.println(i);
+		System.out.println(d.i2); //bad practice, should refernce through class but possible. 
+		System.out.println(d);
+		System.out.println(d.d2); //The object is an instance of the class so can access the class variable.
+		
+		d.i2 = 450;
+		
+		System.out.println(i2);
+		System.out.println(d.i2);
+		
+		Driver drive = new Driver();
+		
+		drive.d2 = 1234;
+		
+		System.out.println(d.d2);
+		System.out.println(drive.d2);
+		System.out.println(d.i2);
+		System.out.println(drive.i2);
+		
+		drive.example(true);
+		
+	}
+	
+	public void example(boolean b) {
+		
+		System.out.println(b);
+		float f = 25.8f;
+		
+		System.out.println(f);
+		
+		if(b) {
+			char c = 'c';
+			System.out.println(c);
+			System.out.println(f);
+		}
+		
+		//System.out.println(c); c is blocked scoped in the if statement. 
+		
+	}
+	
+	
+
+}
