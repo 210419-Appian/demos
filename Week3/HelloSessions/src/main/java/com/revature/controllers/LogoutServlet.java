a372da4e2cde438c8da9c7154a2fe16c21a73e30
gattietime <gattietime@gmail.com>
2021-05-05 15:33:37 -0400

    servlets sessions

---
+++ Week3/HelloSessions/src/main/java/com/revature/controllers/LogoutServlet.java
@@ -0,0 +1,29 @@
+package com.revature.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		HttpSession ses = req.getSession(false);
+		
+		if(ses != null) {
+			ses.invalidate();
+		}
+		//resp.sendRedirect(""); //sends back to the base url of my application. 
+		resp.sendRedirect("http://www.google.com");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		doGet(req, resp);
+	}
+
+}
