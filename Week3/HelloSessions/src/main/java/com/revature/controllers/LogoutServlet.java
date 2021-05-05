031dd1a675cc1c98bfc17853647f3bba747cbd58
gattietime <gattietime@gmail.com>
2021-05-05 16:44:22 -0400

    finished helloSessions

--- Week3/HelloSessions/src/main/java/com/revature/controllers/LogoutServlet.java
+++ Week3/HelloSessions/src/main/java/com/revature/controllers/LogoutServlet.java
@@ -1,5 +1,6 @@
 package com.revature.controllers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -8,22 +9,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LogoutServlet extends HttpServlet{
-	
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.UserDTO;
+
+public class LogoutServlet extends HttpServlet {
+
+	private ObjectMapper om = new ObjectMapper();
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession ses = req.getSession(false);
-		
-		if(ses != null) {
+
+		if (ses != null) {
 			ses.invalidate();
 		}
-		//resp.sendRedirect(""); //sends back to the base url of my application. 
+		// resp.sendRedirect(""); //sends back to the base url of my application.
 		resp.sendRedirect("http://www.google.com");
 	}
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
+	}
+
+	// This is the absolute wrong place to do this. This is a demo.
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession ses = req.getSession(false);
+
+		if (ses != null) {
+			StringBuilder sb = new StringBuilder();
+
+			BufferedReader reader = req.getReader();
+
+			String line = reader.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				// advance to the next line
+				line = reader.readLine();
+			}
+
+			String body = new String(sb);
+
+			UserDTO user = om.readValue(body, UserDTO.class);
+
+			ses.setAttribute("username", user.username);
+			System.out.println(user.password);
+			resp.setStatus(202);
+		} else {
+			resp.sendRedirect("");
+		}
 	}
 
 }
