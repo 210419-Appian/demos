806b7de1e6bf53451ebd5da8e446c0f6a1bb6f80
gattietime <gattietime@gmail.com>
2021-05-06 12:59:07 -0400

    continued Avengers Demo as REST api

--- Week3/HelloFrontController/src/main/java/com/revature/controllers/HomeController.java
+++ Week3/HelloFrontController/src/main/java/com/revature/controllers/HomeController.java
@@ -9,63 +9,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.models.Avenger;
-import com.revature.services.AvengerService;
+import com.revature.models.Home;
+import com.revature.services.HomeService;
 
-public class AvengerController {
+public class HomeController {
 
-	private AvengerService avService = new AvengerService();
+	private HomeService homeServ = new HomeService();
 	private ObjectMapper om = new ObjectMapper();
 
-	public void getAllAvengers(HttpServletResponse resp) throws IOException {
-		// Gets my avengers from the db.
-		List<Avenger> list = avService.avengersAssemble();
+	public void getHome(HttpServletResponse resp, String string) throws IOException {
+		Home h = homeServ.findHomeByName(string);
 
 		// Convert Java object into a JSON string that can be written to the body of an
 		// HTTP response
+		String json = om.writeValueAsString(h);
+		System.out.println(json);
+		PrintWriter pw = resp.getWriter();
+		pw.print(json);
+		resp.setStatus(200);
+
+	}
+
+	public void getAllHomes(HttpServletResponse resp) throws IOException {
+		List<Home> list = homeServ.getAllHomes();
 		String json = om.writeValueAsString(list);
 		System.out.println(json);
 		PrintWriter pw = resp.getWriter();
 		pw.print(json);
 		resp.setStatus(200);
 	}
-	
-	public void getAvenger(HttpServletResponse resp, int id) throws IOException{
-		
-		Avenger a = avService.getOneAvenger(id);
 
-		// Convert Java object into a JSON string that can be written to the body of an
-		// HTTP response
-		String json = om.writeValueAsString(a);
-		System.out.println(json);
-		PrintWriter pw = resp.getWriter();
-		pw.print(json);
-		resp.setStatus(200);
-	}
-
-	public void addAvenger(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void addHome(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		BufferedReader reader = req.getReader();
-		
+
 		StringBuilder sb = new StringBuilder();
-		
+
 		String line = reader.readLine();
-		
-		while(line != null) {
+
+		while (line != null) {
 			sb.append(line);
 			line = reader.readLine();
 		}
-		
+
 		String body = new String(sb);
+
+		Home home = om.readValue(body, Home.class);
 		
-		Avenger a = om.readValue(body, Avenger.class);
-		
-		if(avService.addAvenger(a)) {
+		if(homeServ.createHome(home)) {
 			resp.setStatus(201);
 		}else {
 			resp.setStatus(406);
 		}
-		
-		
 	}
 
 }
--- Week3/HelloFrontController/src/main/java/com/revature/web/FrontControllerServlet.java
+++ Week3/HelloFrontController/src/main/java/com/revature/web/FrontControllerServlet.java
@@ -9,45 +9,69 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.revature.controllers.AvengerController;
+import com.revature.controllers.HomeController;
 
-public class FrontControllerServlet extends HttpServlet{
-	
+public class FrontControllerServlet extends HttpServlet {
+
 	private String BaseURL = null;
 	private AvengerController avControl = new AvengerController();
-	
+	private HomeController homeControl = new HomeController();
+
 	@Override
-	public void init(ServletConfig config) throws ServletException{
+	public void init(ServletConfig config) throws ServletException {
 		BaseURL = config.getInitParameter("BaseURL");
 	}
-	
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("application/json");
-		
-		resp.setStatus(404); //default if someone sends a malformed request. 
-		
+
+		resp.setStatus(404); // default if someone sends a malformed request.
+
 		final String URL = req.getRequestURI().replace(BaseURL, "");
-		
+
 		System.out.println(URL);
-		
-		//A path variable is a way to pass information about the request in the URL itself. Generally you have
-		//a final / in the url that takes a variable input. i.e. avenger/1 will get you the first avenger while 
-		// avenger/2 gets the second etc. 
-		
+
+		// A path variable is a way to pass information about the request in the URL
+		// itself. Generally you have
+		// a final / in the url that takes a variable input. i.e. avenger/1 will get you
+		// the first avenger while
+		// avenger/2 gets the second etc.
+
 		String[] sections = URL.split("/");
-		
+
 		System.out.println(sections);
-		
-		switch(sections[0]) {
-		case "avenger":
-			if(sections.length == 2) {
-				int id = Integer.parseInt(sections[1]);
-				avControl.getAvenger(resp, id);
-			}else {
-				avControl.getAllAvengers(resp);
+
+		switch (sections[0]) {
+		case "avengers":
+			if (req.getMethod().equals("GET")) {
+				if (sections.length == 2) {
+					int id = Integer.parseInt(sections[1]);
+					avControl.getAvenger(resp, id);
+				} else {
+					avControl.getAllAvengers(resp);
+				}
+			} else if (req.getMethod().equals("POST")) {
+				avControl.addAvenger(req, resp);
+			}
+			break;
+		case "homes":
+			if (req.getMethod().equals("GET")) {
+				if (sections.length == 2) {
+					homeControl.getHome(resp, sections[1]);
+				} else {
+					homeControl.getAllHomes(resp);
+				}
+			}else if(req.getMethod().equals("POST")) {
+				homeControl.addHome(req, resp);
 			}
 		}
-		
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
 	}
 
 }
